e50b806e5b21011c35dd63a5a30b0d570dc189aa
xingzhe <asd_email@163.com>
2019-06-13 17:04:46 +0800

    node blog init

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const Koa = require('koa')
 const fs = require('fs')
 const app = new Koa()
 const router = require('./routers/index')
+const config = require('./config/index')
+
+const bodyParser = require('koa-bodyparser');
+const ejs = require('ejs');
+const session = require('koa-session-minimal');
+const MysqlStore = require('koa-mysql-session');
+const views = require('koa-views')
+const staticCache = require('koa-static-cache')
 
 app.use(router.routes()).use(router.allowedMethods())
 
