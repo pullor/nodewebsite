d33226ddfb39cb7153f1232ecd3a95e9eb6a4e71
xingzhe <asd_email@163.com>
2019-10-12 15:53:33 +0800

    Revert "版本回退到b547599"
    This reverts commit 0e284a4dafe97b554041c591e9bd9f1601c78448.

--- publish.sh
+++ publish.sh
@@ -1,1 +1,25 @@
-#sdfjasld
+#build
+echo 'start...build prod!!!'
+npm run build:prod
+echo 'end...build prod!!!'
+
+echo 'starting!!!replace static files...'
+python upqiniu.py
+echo 'end!!!replace static files...'
+
+hosts=(
+# 118.25.134.254
+# 118.25.95.129
+# 129.211.15.136
+111.231.137.247
+)
+
+TEST_DIR=/Users/tianmeng/Desktop/Weixu/weixu-h5/dist/
+SERVER_DIR=/var/www/weixu-f2e-H5-activity/laud/v2
+
+for(( i=0;i<${#hosts[@]};i++)) 
+do
+	echo ${hosts[i]};
+	rsync -apCvq -e 'ssh -p 10022' ${TEST_DIR} root@${hosts[i]}:${SERVER_DIR}
+	echo 'rsync success'
+done;
